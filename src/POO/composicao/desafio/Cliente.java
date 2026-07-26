b61da282e9de0f21058185337ec6e6c29d727b68
Luiggi <LuiggiOliveira@users.noreply.github.com>
2026-07-26 17:31:42 -0300

    refactor: troquei ArrayList<> por List<>, implementei novamente os métodos para obter total de cada compra e de todas as compras do cliente, além de alinhar melhor a tabela porque estava dando agonia.

--- src/POO/composicao/desafio/Cliente.java
+++ src/POO/composicao/desafio/Cliente.java
@@ -1,10 +1,13 @@
 package POO.composicao.desafio;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Cliente {
 
-    final ArrayList<Compra> compras = new ArrayList<>();
+//    Observação: eu mudei para a interface List ao invés de ArrayList para seguir as boas práticas de programação, permitindo
+//    trocar a implementação interna sem quebrar o código, mantendo o baixo acoplamento
+    final List<Compra> compras = new ArrayList<>();
     final String nome;
 
     Cliente(String nome){
@@ -17,26 +20,33 @@ public class Cliente {
         System.out.println("Compra adicionada à lista de compras do cliente.");
     }
 
+    double getTotalCliente(){
+        double total = 0;
+
+        for (Compra compra: compras){
+            total += compra.getTotalCompra();
+        }
+
+        return total;
+    }
+
     void detalharCliente(){
         int id_compra = 1;
-        double total_cliente = 0;
 
         System.out.println("*** " + this.nome + " *** ");
-        System.out.printf("%-5s | %-18s | %-10s | %-12s | %-12s%n", //obs: recomenda-se usar %n ao invés de /n para manter consistência
+        System.out.printf("%-6s | %-15s | %-10s | %-13s | %-12s%n", //obs: recomenda-se usar %n ao invés de /n para manter consistência
                 "ID_COMPRA", "NOME_PRODUTO", "QUANTIDADE", "PREÇO (R$)", "TOTAL_ITEM (R$)");
 
         for (Compra compra: compras){
             for (Item item: compra.itens){
                 double total_item = item.quantidade * item.produto.preco;
-                total_cliente += total_item;
-
-                System.out.printf("▷  %-4d | %-20s | %-10d | R$ %-10.2f | R$ %-10.2f%n",
+                System.out.printf("▷  %-6d | %-15s | %-10d | R$ %-10.2f | R$ %-8.2f%n",
                         id_compra, item.produto.nome, item.quantidade, item.produto.preco, total_item);
             }
             id_compra++;
         }
 
-        System.out.println("→ Total gasto pelo cliente: R$" + total_cliente);
+        System.out.println("→ Total gasto pelo cliente: R$" + this.getTotalCliente());
     }
 
 }
--- src/POO/composicao/desafio/Compra.java
+++ src/POO/composicao/desafio/Compra.java
@@ -1,13 +1,24 @@
 package POO.composicao.desafio;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Compra {
 
-    final ArrayList<Item> itens = new ArrayList<>();
+    final List<Item> itens = new ArrayList<>();
 
     void adicionarItem(Produto produto, int quantidade){
         this.itens.add(new Item(produto, quantidade));
         System.out.println("Item adicionado à lista de itens.");
+    }
+
+    double getTotalCompra(){
+        double total = 0;
+
+        for (Item item: itens){
+            total += item.quantidade * item.produto.preco;
+        }
+
+        return total;
     }
 }
